2e0221bc4554730318d7eb21a0fe153b15a514aa
Cindy Tai <60444356+htai327@users.noreply.github.com>
2020-08-12 12:43:32 -0400

    Upload all relevant files
    Upload assignment, client/server code, output, and wireshark analysis.

---
+++ dnsserver.py
@@ -0,0 +1,47 @@
+# Cindy Tai 31462986 Section 2
+
+#! /usr/bin/env python3
+# DNS Server
+import sys
+import struct
+import socket
+import time
+import random
+
+# Get server IP address and port from command line args
+addressIP = sys.argv[1]
+port = int(sys.argv[2])
+
+# Create a UDP server socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Assign IP address and port to socket
+serverSocket.bind((addressIP, port))
+
+print("The server is ready to receive on port:  " + str(port) + "\n")
+
+while True:
+    message, address = serverSocket.recvfrom(1024)
+    msg = struct.unpack('>hhihh{}s'.format(len(message) - 12), message)
+    msgID = int(msg[2])
+    qLen = int(msg[3])
+    question = msg[5].decode()
+    rCode = 1
+    answer = ""
+    records = []
+    ansFound = False
+
+    file = open("dns-master.txt", "r")
+    for line in file:
+        if 'A IN' in line:
+            records.append(line.rstrip())
+
+    for record in records:
+        if question in record:
+            answer = record
+            ansFound = True
+
+    if (ansFound == True):
+        rCode = 0
+
+    response = struct.pack('>hhihh{}s{}s'.format(len(question.encode()), len(answer.encode())), 2, rCode, msgID, len(question), len(answer), question.encode(), answer.encode())
+    serverSocket.sendto(response, address)
